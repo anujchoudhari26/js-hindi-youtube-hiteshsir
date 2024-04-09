166709431e13ee732c28d8ae51950100e37bf238
anujchoudhari26 <98764791+anujchoudhari26@users.noreply.github.com>
2024-04-09 12:22:32 +0000

    Updated VSCode Config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -51,8 +51,8 @@ let str3 = str1 + str2
 // console.log("1" + 2 + 2);
 // console.log(1 + 2 + "2");
 
-// console.log( (3 + 4) * 5 % 3);
-
+console.log( (3 + 4) * 5 % 3);
+console.log( (1 + 1) * 2 % 2);
 // console.log(+true);
 // console.log(+"");
 
